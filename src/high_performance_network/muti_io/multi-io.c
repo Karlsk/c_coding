5d3b20cac7645597d4fc57573dd7bb0210081e7c
Karlsk <gao060227@outlook.com>
2024-10-06 15:09:27 +0800

    use select to multi con

--- src/high_performance_network/muti_io/multi-io.c
+++ src/high_performance_network/muti_io/multi-io.c
@@ -9,10 +9,13 @@
 #include <netinet/in.h>
 
 #include <pthread.h>
+#include <sys/select.h>
 
 #define TCP_PORT 9999 
 #define ENABLE_NOBLOCK 0
-#define ENABLE_MUTITHREAD 1
+#define ENABLE_MUTITHREAD 0
+#define ENABLE_SELECT 1
+
 
 #define BUFFER_LEN 1024 
 
@@ -60,6 +63,7 @@ int main(int argc, char const *argv[])
     struct sockaddr_in  clientaddr;
     memset(&clientaddr, 0, sizeof(struct sockaddr_in));
     socklen_t len  = sizeof(struct sockaddr_in);
+#if 0
     int clientfd = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
     while(1) {
         int clientfd = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
@@ -67,12 +71,58 @@ int main(int argc, char const *argv[])
         pthread_t threadid;
         pthread_create(&threadid, NULL, client_thread, &clientfd); 
 #else
+
         char buffer[BUFFER_LEN] = {0};
         int ret = recv(clientfd, buffer, BUFFER_LEN, 0);
         printf("ret: %d, bufer: %s\n", ret, buffer);
         send(clientfd, buffer, ret, 0);
+}
 #endif
+
+#elif ENABLE_SELECT // select 
+    //select(maxfd，rfds, wfds, efds, timeout);
+    fd_set rfds, rset;
+    FD_ZERO(&rfds);
+    FD_SET(sockfd, &rfds);
+
+    int maxfd = sockfd;
+    int clientfd = 0;
+    while(1)
+    {
+        rset = rfds;
+        int nready = select(maxfd+1, &rset, NULL, NULL, NULL);
+        if (FD_ISSET(sockfd, &rset))
+        {
+            clientfd = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
+            printf("clientfd: %d\n", clientfd);
+            FD_SET(clientfd, &rfds);
+            if (clientfd > maxfd) {
+                maxfd = clientfd;
+            }
+            if (--nready == 0) continue;
+        }
+        int i = 0;
+        for (i = sockfd+1; i <= maxfd; i++)
+        {
+            if (FD_ISSET(i, &rset))
+            {
+                char buffer[BUFFER_LEN] = {0};
+                int ret = recv(i, buffer, BUFFER_LEN, 0);
+                if (ret == 0)
+                {
+                    close(i);
+                    break;
+                }
+                printf("ret: %d, bufer: %s\n", ret, buffer);
+                send(i, buffer, ret, 0);
+            }
+            
+        }
+        
+
     }
+#endif
+    
     close(sockfd);
     return 0;
 }
